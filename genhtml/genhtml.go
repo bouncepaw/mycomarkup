a63370acadcd31d26fb388e515c55ebe49965afd
Timur Ismagilov <bouncepaw2@ya.ru>
2021-10-02 00:57:39 +0300

    Add that tag kind thing

--- genhtml/genhtml.go
+++ genhtml/genhtml.go
@@ -14,10 +14,22 @@ import (
 // We might change this later or provide a way to change by the user.
 const indentation = "\t"
 
+// TagKind is the kind of a Tag. The way the tag is rendered depends on the kind.
+type TagKind int
+
+const (
+	// ClosedTag is a tag that looks like that: <t>children</t>.
+	ClosedTag TagKind = iota
+	// UnclosedTag is a tag that looks like that: <t/>
+	UnclosedTag
+	// WrapperTag is a tag that looks like that: children
+	WrapperTag
+)
+
 // Tag represents an HTML tag/DOM node.
 type Tag struct {
 	Name       string
-	IsClosed   bool
+	Kind       TagKind
 	Attributes map[string]string
 	// nil if infertile
 	Children []Tag
@@ -25,20 +37,32 @@ type Tag struct {
 
 // String returns an indented pretty-printed representation of the Tag.
 func (t Tag) String() (res string) {
-	if !t.IsClosed {
+	switch t.Kind {
+	case UnclosedTag:
 		return fmt.Sprintf("<%s%s/>\n", t.Name, attrs(t.Attributes))
-	}
-	res += fmt.Sprintf("<%s%s>\n", t.Name, attrs(t.Attributes))
-	var tmp string
-	for i, child := range t.Children {
-		if i > 0 {
-			tmp += "\n"
+	case ClosedTag:
+		res += fmt.Sprintf("<%s%s>\n", t.Name, attrs(t.Attributes))
+		var tmp string
+		for i, child := range t.Children {
+			if i > 0 {
+				tmp += "\n"
+			}
+			tmp += child.String()
 		}
-		tmp += child.String()
+		res += eachLineIndented(tmp)
+		res += fmt.Sprintf("</%s>", t.Name)
+		return res
+	case WrapperTag:
+		for i, child := range t.Children {
+			if i > 0 {
+				res += "\n"
+			}
+			res += child.String()
+		}
+		return res
+	default:
+		return "ERROR"
 	}
-	res += eachLineIndented(tmp)
-	res += fmt.Sprintf("</%s>", t.Name)
-	return res
 }
 
 func attrs(m map[string]string) (res string) {
@@ -65,14 +89,14 @@ func BlockToTag(ctx mycocontext.Context, block blocks.Block, counter *blocks.IDC
 	case blocks.HorizontalLine:
 		return Tag{
 			Name:       "hr",
-			IsClosed:   false,
+			Kind:       UnclosedTag,
 			Attributes: attrs,
 			Children:   nil,
 		}
 	default:
 		return Tag{
 			Name:       "error",
-			IsClosed:   false,
+			Kind:       UnclosedTag,
 			Attributes: nil,
 			Children:   nil,
 		}
